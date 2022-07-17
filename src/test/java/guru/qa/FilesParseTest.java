5ae279dd3a895b94a2761d6b818b60d605f23440
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-07-17 23:11:27 +0300

    +added classwork material

--- src/test/java/guru/qa/FilesParseTest.java
+++ src/test/java/guru/qa/FilesParseTest.java
@@ -1,4 +1,72 @@
 package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.CsvParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 //
 public class FilesParseTest {
+
+    ClassLoader classLoader = FilesParseTest.class.getClassLoader();
+
+    @Test
+    void pdfTest() throws Exception {
+        Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
+        File file = $("a[href*='junit-user-guide-5.8.2.pdf']").download();
+        PDF pdf = new PDF(file);
+        assertThat(pdf.author).isEqualTo("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes," +
+                " Marc Philipp, Juliette de Rancourt, Christian Stein");
+    }
+
+    @Test
+    void xlsTest() throws Exception {
+        Selenide.open("http://romashka2008.ru/price");
+        File file = $(".site-content__right a[href*='/f/prajs_ot_0807.xls']").download();
+        XLS xls = new XLS(file);
+        assertThat(xls.excel.getSheetAt(0)
+                .getRow(22)
+                .getCell(2)
+                .getStringCellValue())
+                .contains("БОЛЬШАЯ РАСПРОДАЖА");
+    }
+
+    @Test
+    void csvTest() throws Exception {
+        InputStream is = classLoader.getResourceAsStream("example.csv");
+        CSVReader csvReader = new CSVReader(new InputStreamReader(is, UTF_8));
+        List<String[]> csv = csvReader.readAll();
+        assertThat(csv).contains(
+                new String[] {"teacher","lesson","date"},
+                new String[] {"Tuchs","junit","03.06"},
+                new String[] {"Eroshenko","allure","07.06"}
+        );
+    }
+
+    @Test
+    void zipTest() {
+
+    }
+
+    @Test
+    void jsonTest() {
+
+    }
+
+
 }
